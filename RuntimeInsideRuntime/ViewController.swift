c9bfe997cc1573c78fb1f8b990d35c40e9e87e92
Denis Morozov <kramig@yandex-team.ru>
2022-07-10 20:58:17 +0300

    Initial Commit

---
+++ RuntimeInsideRuntime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RuntimeInsideRuntime
+//
+//  Created by Denis S. Morozov on 10.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
